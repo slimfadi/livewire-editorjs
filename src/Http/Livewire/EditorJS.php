3f1581a65737ae814d876ab86db89168bb5eaf54
Fadi Hassan <ffa.fadi@gmail.com>
2025-04-15 02:49:04 +0300

    Update EditorJS.php

--- src/Http/Livewire/EditorJS.php
+++ src/Http/Livewire/EditorJS.php
@@ -104,7 +104,7 @@ class EditorJS extends Component
 
     public function save()
     {
-        $this->emitUp("editorjs-save:{$this->editorId}", $this->data);
+        $this->emit("editorjs-save:{$this->editorId}", $this->data);
     }
 
     public function render()
